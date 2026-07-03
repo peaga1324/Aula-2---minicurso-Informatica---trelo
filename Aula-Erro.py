ecf835b54aead57db3bf96c3f91be3c557dd461c
p1ve7e <gabrielnascimento123@outlook.com>
2026-07-03 19:39:11 -0300

    Create Aula-Erro.py

---
+++ Aula-Erro.py
@@ -0,0 +1,49 @@
+# Calculadora de salário
+# ESTE CÓDIGO POSSUI VÁRIOS ERROS INTENCIONAIS
+
+print("=== Cálculo de Salário ==="
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: ")
+
+horas = float(input("Horas trabalhadas: "))
+valor_hora = float(input("Valor por hora: "))
+
+salario = horas * valorHora
+
+bonus = salario * 0,10
+
+salario_final = salario + bonu
+
+print("")
+
+print("Funcionário:", Nome)
+print("Idade:", idade)
+
+print("Salário:", salario)
+print("Bônus:", bonus)
+print("Salário Final:", salarioFinal)
+
+desconto = input("Digite o valor do desconto: ")
+
+resultado = salario_final - desconto
+
+print("Salário com desconto:", resultado
+
+extra = float(input("Horas extras: "))
+
+valor_extra = extra * valor_horas
+
+total = resultado + valor_extra
+
+print("Pagamento total:", Total)
+
+empresa = input("Nome da empresa: ")
+
+print("Empresa:", empresa)
+
+mes = input("Mês de referência: ")
+
+print("Funcionário", nome "receberá", total, "no mês de", mes)
+
+print(Fim do programa!)
